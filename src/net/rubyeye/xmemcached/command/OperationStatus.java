1b4772445aae0ee0826ced0b353d51ff43810f77
killme2008 <killme2008@gmail.com>
2009-06-02 12:54:55 +0000

    delete old structure

--- src/net/rubyeye/xmemcached/command/OperationStatus.java
+++
@@ -1,5 +0,0 @@
-package net.rubyeye.xmemcached.command;
-
-public enum OperationStatus {
-	SENDING,WRITING, SENT, PROCESSING, DONE, CANCEL;
-}
--- src/net/rubyeye/xmemcached/test/unittest/MemcachedHandlerTest.java
+++
@@ -1,5 +0,0 @@
-package net.rubyeye.xmemcached.test.unittest;
-
-public class MemcachedHandlerTest {
-	
-}
