f19d47c5c73719ae3d01ad54a0bcf69dc198605b
killme2008 <killme2008@gmail.com>
2009-07-28 10:32:44 +0000

    Supports hibernate-memcached.Issue 41.Need to be tested.

---
+++ src/main/java/net/rubyeye/xmemcached/utils/hibernate/Xmemcache.java
@@ -0,0 +1,94 @@
+package net.rubyeye.xmemcached.utils.hibernate;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.exception.MemcachedException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.googlecode.hibernate.memcached.LoggingMemcacheExceptionHandler;
+import com.googlecode.hibernate.memcached.Memcache;
+import com.googlecode.hibernate.memcached.MemcacheExceptionHandler;
+import com.googlecode.hibernate.memcached.utils.StringUtils;
+
+public class Xmemcache implements Memcache {
+	private static final Logger log = LoggerFactory.getLogger(Xmemcache.class);
+	private MemcacheExceptionHandler exceptionHandler = new LoggingMemcacheExceptionHandler();
+
+	private final MemcachedClient memcachedClient;
+
+	public Xmemcache(MemcachedClient memcachedClient) {
+		this.memcachedClient = memcachedClient;
+	}
+
+	public Object get(String key) {
+		try {
+			log.debug("MemcachedClient.get({})", key);
+			return this.memcachedClient.get(key);
+		} catch (Exception e) {
+			this.exceptionHandler.handleErrorOnGet(key, e);
+		}
+		return null;
+	}
+
+	public Map<String, Object> getMulti(String... keys) {
+		try {
+			return this.memcachedClient.get(Arrays.asList(keys));
+		} catch (Exception e) {
+			this.exceptionHandler.handleErrorOnGet(
+					StringUtils.join(keys, ", "), e);
+		}
+		return null;
+	}
+
+	public void set(String key, int cacheTimeSeconds, Object o) {
+		log.debug("MemcachedClient.set({})", key);
+		try {
+			this.memcachedClient.set(key, cacheTimeSeconds, o);
+		} catch (Exception e) {
+			this.exceptionHandler.handleErrorOnSet(key, cacheTimeSeconds, o, e);
+		}
+	}
+
+	public void delete(String key) {
+		try {
+			this.memcachedClient.delete(key);
+		} catch (Exception e) {
+			this.exceptionHandler.handleErrorOnDelete(key, e);
+		}
+	}
+
+	public void incr(String key, int factor, int startingValue) {
+		try {
+			this.memcachedClient.incr(key, factor);
+		} catch (MemcachedException e) {
+			try {
+				this.memcachedClient.add(key, 0, String.valueOf(startingValue));
+				this.memcachedClient.incr(key, factor);
+			} catch (Exception ex) {
+				this.exceptionHandler.handleErrorOnIncr(key, factor,
+						startingValue, ex);
+			}
+		} catch (Exception e) {
+			this.exceptionHandler.handleErrorOnIncr(key, factor, startingValue,
+					e);
+		}
+	}
+
+	public void shutdown() {
+		log.debug("Shutting down spy MemcachedClient");
+		try {
+			this.memcachedClient.shutdown();
+		} catch (Exception e) {
+			log.error("Shut down MemcachedClient error", e);
+		}
+
+	}
+
+	public void setExceptionHandler(MemcacheExceptionHandler exceptionHandler) {
+		this.exceptionHandler = exceptionHandler;
+	}
+}
---
+++ src/main/java/net/rubyeye/xmemcached/utils/hibernate/XmemcachedClientFactory.java
@@ -0,0 +1,124 @@
+package net.rubyeye.xmemcached.utils.hibernate;
+
+import net.rubyeye.xmemcached.CommandFactory;
+import net.rubyeye.xmemcached.HashAlgorithm;
+import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.MemcachedSessionLocator;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.command.BinaryCommandFactory;
+import net.rubyeye.xmemcached.command.TextCommandFactory;
+import net.rubyeye.xmemcached.impl.ArrayMemcachedSessionLocator;
+import net.rubyeye.xmemcached.impl.KetamaMemcachedSessionLocator;
+import net.rubyeye.xmemcached.utils.AddrUtil;
+
+import com.googlecode.hibernate.memcached.Config;
+import com.googlecode.hibernate.memcached.Memcache;
+import com.googlecode.hibernate.memcached.MemcacheClientFactory;
+import com.googlecode.hibernate.memcached.PropertiesHelper;
+
+/**
+ * Parses hibernate properties to produce a MemcachedClient.<br/>
+ * See {@link com.googlecode.hibernate.memcached.MemcachedCacheProvider} for
+ * property details. SPI for xmemcached.
+ * 
+ * @author dennis
+ */
+public class XmemcachedClientFactory implements MemcacheClientFactory {
+
+	public static final String PROP_SERVERS = Config.PROP_PREFIX + "servers";
+	public static final String PROP_OPERATION_QUEUE_LENGTH = Config.PROP_PREFIX
+			+ "operationQueueLength";
+	public static final String PROP_READ_BUFFER_SIZE = Config.PROP_PREFIX
+			+ "readBufferSize";
+	public static final String PROP_OPERATION_TIMEOUT = Config.PROP_PREFIX
+			+ "operationTimeout";
+	public static final String PROP_HASH_ALGORITHM = Config.PROP_PREFIX
+			+ "hashAlgorithm";
+	public static final String PROP_COMMAND_FACTORY = Config.PROP_PREFIX
+			+ "commandFactory";
+
+	public static final String PROP_SESSION_LOCATOR = Config.PROP_PREFIX
+			+ "sessionLocator";
+
+	private final PropertiesHelper properties;
+
+	public XmemcachedClientFactory(PropertiesHelper properties) {
+		this.properties = properties;
+	}
+
+	public Memcache createMemcacheClient() throws Exception {
+		MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil
+				.getAddresses(getServerList()));
+		builder.setCommandFactory(getCommandFactory());
+		builder.setSessionLocator(getSessionLocator());
+		builder.getConfiguration()
+				.setSessionReadBufferSize(getReadBufferSize());
+		MemcachedClient client = builder.build();
+		client.setOpTimeout(getOperationTimeoutMillis());
+		return new Xmemcache(client);
+	}
+
+	protected MemcachedSessionLocator getSessionLocator() {
+		if (clazzNameEquals(ArrayMemcachedSessionLocator.class)) {
+			return new ArrayMemcachedSessionLocator(getHashAlgorithm());
+		}
+
+		if (clazzNameEquals(KetamaMemcachedSessionLocator.class)) {
+			return new KetamaMemcachedSessionLocator(getHashAlgorithm());
+		}
+
+		throw new IllegalArgumentException("Unsupported "
+				+ PROP_SESSION_LOCATOR + " value: " + getCommandFactoryName());
+	}
+
+	protected CommandFactory getCommandFactory() {
+		if (clazzNameEquals(TextCommandFactory.class)) {
+			return new TextCommandFactory();
+		}
+
+		if (clazzNameEquals(BinaryCommandFactory.class)) {
+			return new BinaryCommandFactory();
+		}
+
+		throw new IllegalArgumentException("Unsupported "
+				+ PROP_COMMAND_FACTORY + " value: " + getCommandFactoryName());
+	}
+
+	private boolean clazzNameEquals(Class<?> cls) {
+		return cls.getSimpleName().equals(getCommandFactoryName());
+	}
+
+	public String getServerList() {
+		return this.properties.get(PROP_SERVERS, "localhost:11211");
+	}
+
+	public int getReadBufferSize() {
+		return this.properties.getInt(PROP_READ_BUFFER_SIZE,
+				MemcachedClient.DEFAULT_SESSION_READ_BUFF_SIZE);
+	}
+
+	public long getOperationTimeoutMillis() {
+		return this.properties.getLong(PROP_OPERATION_TIMEOUT,
+				MemcachedClient.DEFAULT_OP_TIMEOUT);
+	}
+
+	public HashAlgorithm getHashAlgorithm() {
+		return this.properties.getEnum(PROP_HASH_ALGORITHM,
+				HashAlgorithm.class, HashAlgorithm.NATIVE_HASH);
+	}
+
+	public String getCommandFactoryName() {
+		return this.properties.get(PROP_COMMAND_FACTORY,
+				TextCommandFactory.class.getSimpleName());
+	}
+
+	public String getSessionLocatorName() {
+		return this.properties.get(PROP_SESSION_LOCATOR,
+				ArrayMemcachedSessionLocator.class.getSimpleName());
+	}
+
+	protected PropertiesHelper getProperties() {
+		return this.properties;
+	}
+}
