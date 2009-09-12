e1653d2b5a0a7e812b813912465adf6ff9d70253
killme2008 <killme2008@gmail.com>
2009-09-12 16:47:40 +0000

    Add connection pool client unit test.

---
+++ src/test/java/net/rubyeye/xmemcached/test/unittest/ConnectionPoolMemcachedClientTest.java
@@ -0,0 +1,41 @@
+package net.rubyeye.xmemcached.test.unittest;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.command.BinaryCommandFactory;
+import net.rubyeye.xmemcached.impl.KetamaMemcachedSessionLocator;
+import net.rubyeye.xmemcached.utils.AddrUtil;
+
+public class ConnectionPoolMemcachedClientTest extends XMemcachedClientTest {
+	private static final int CONNECTION_POOL_SIZE = 3;
+
+	@Override
+	public MemcachedClientBuilder createBuilder() throws Exception {
+		MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil
+				.getAddresses(this.properties
+						.getProperty("test.memcached.servers")));
+		builder.setConnectionPoolSize(CONNECTION_POOL_SIZE);
+		return builder;
+	}
+
+	@Override
+	public MemcachedClientBuilder createWeightedBuilder() throws Exception {
+		List<InetSocketAddress> addressList = AddrUtil
+				.getAddresses(this.properties
+						.getProperty("test.memcached.servers"));
+		int[] weights = new int[addressList.size()];
+		for (int i = 0; i < weights.length; i++) {
+			weights[i] = i + 1;
+		}
+
+		MemcachedClientBuilder builder = new XMemcachedClientBuilder(
+				addressList, weights);
+		builder.setConnectionPoolSize(CONNECTION_POOL_SIZE);
+		builder.setCommandFactory(new BinaryCommandFactory());
+		builder.setSessionLocator(new KetamaMemcachedSessionLocator());
+		return builder;
+	}
+}
