fec5cd914939cb245a4dc517f19c30e46da96ec5
killme2008 <killme2008@gmail.com>
2009-09-17 06:53:04 +0000

    Modify README

--- src/test/java/net/rubyeye/xmemcached/example/KestrelExample.java
+++ src/test/java/net/rubyeye/xmemcached/example/KestrelExample.java
@@ -3,16 +3,22 @@ package net.rubyeye.xmemcached.example;
 import net.rubyeye.xmemcached.MemcachedClient;
 import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.impl.KetamaMemcachedSessionLocator;
+import java.util.*;
 
 public class KestrelExample {
-  public static void main(String []args)throws Exception{
-	  XMemcachedClientBuilder builder=new XMemcachedClientBuilder();
-	  builder.setSessionLocator(new KetamaMemcachedSessionLocator());
-	  MemcachedClient xmc =builder.build();
-	  xmc.addServer("61.152.188.80",22133);
-	  xmc.setOptimizeGet(false);
-	  xmc.setOpTimeout(60000);
-	  System.out.println(xmc.get("test_queue_2"));//正常
-	  System.out.println(xmc.get("test_queue/t=250"));//异常
-  }
+	public static void main(String[] args) throws Exception {
+		XMemcachedClientBuilder builder = new XMemcachedClientBuilder();
+		builder.setSessionLocator(new KetamaMemcachedSessionLocator());
+		MemcachedClient xmc = builder.build();
+		xmc.addServer("localhost", 22133);
+		xmc.setOpTimeout(60000);
+
+		System.out.println(xmc.get("test_queue/t=250"));
+		xmc.set("test_queue", 0, "test");
+		System.out.println(xmc.get("test_queue"));// 正常
+		xmc.set("test_queue", 200, new ArrayList());
+		xmc.get("test_queue/open");
+		xmc.get("test_queue/close");
+		xmc.shutdown();
+	}
 }
