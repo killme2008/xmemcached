3acc5091c5e8c67121e0f4033fd6ddaf4c4d4f48
killme2008 <killme2008@gmail.com>
2010-10-16 16:17:25 +0000

    Modify count to 40000

--- benchmark/src/net/rubyeye/memcached/benchmark/Constants.java
+++ benchmark/src/net/rubyeye/memcached/benchmark/Constants.java
@@ -9,7 +9,7 @@ public interface Constants {
 	// 128,
 			64, 512, 1024, 4096, 16 * 1024, };
 	public static final double WRITE_RATE = 0.20;
-	final int BASE_REPEATS = 10000;
+	final int BASE_REPEATS = 40000;
 	final long OP_TIMEOUT = 5000;
 
 }
