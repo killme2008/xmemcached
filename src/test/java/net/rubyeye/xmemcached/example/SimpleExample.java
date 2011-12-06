482f1d6e00b215572b7c894879bfafee3a316881
dennis zhuang <killme2008@gmail.com>
2011-12-06 21:41:35 +0800

    Fixed issue 169

--- src/test/java/net/rubyeye/xmemcached/example/SimpleExample.java
+++ src/test/java/net/rubyeye/xmemcached/example/SimpleExample.java
@@ -6,9 +6,12 @@ import java.util.concurrent.TimeoutException;
 import net.rubyeye.xmemcached.KeyIterator;
 import net.rubyeye.xmemcached.MemcachedClient;
 import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClient;
 import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.command.BinaryCommandFactory;
 import net.rubyeye.xmemcached.exception.MemcachedException;
 import net.rubyeye.xmemcached.utils.AddrUtil;
+
 
 /**
  * Simple example for xmemcached
@@ -17,68 +20,66 @@ import net.rubyeye.xmemcached.utils.AddrUtil;
  * 
  */
 public class SimpleExample {
-	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.err.println("Useage:java SimpleExample [servers]");
-			System.exit(1);
-		}
-		MemcachedClient memcachedClient = getMemcachedClient(args[0]);
-		if (memcachedClient == null) {
-			throw new NullPointerException(
-					"Null MemcachedClient,please check memcached has been started");
-		}
-		try {
-			// add a,b,c
-			System.out.println("Add a,b,c");
-			memcachedClient.set("a", 0, "Hello,xmemcached");
-			memcachedClient.set("b", 0, "Hello,xmemcached");
-			memcachedClient.set("c", 0, "Hello,xmemcached");
-			// get a
-			String value = memcachedClient.get("a");
-			System.out.println("get a=" + value);
-			System.out.println("delete a");
-			// delete a
-			memcachedClient.delete("a");
-			// reget a
-			value = memcachedClient.get("a");
-			System.out.println("after delete,a=" + value);
+    static String specilaKey =
+            "emhhbmd5b25nYmluIDAwMTEzMDI3O2ZhbndlbmthaSAwMDE1MjkxOTt4aWFvcGVuZyA2MTA3OTt3YW5nZmVuZyA1OTQ2NTtjdWljaGFuZ3poaSAwMDE1MDIwMjtzdW5qaWUgMDAxMTUxMjc7Z3VvcGVuZ2JvIDAwMTEyOTQxO2xpbnl1biA2MTAwNDtsaXV5b25nIDAwMTYwNTM3O2xpdXpoZW5ncm9uZyAwMDEyMTAyNTt0dXhpYSAwMDE1Mjg3MDtkZW5ncm9uZyA1NTE0MztjaGVuamluIDAwMTM0MTk1O2xpa2VqaW5nIDAwMTIxOTczO2h1YW5nY2hlbmdkZSAwMDE2ODk5Njt6aGFuZ2ZhbiAwMDE2MzYxMzt0dW1pbmhhaSAwMDExODcwNTtwZWlqaW5nIDAwMTA1Mzc2O2x2amlhbmhvbmcgNjg2Njk7bHVvZnVyb25nIDY5NTE3O2xpdXRpZWp1biAwMDExNDQ5NztoYW54aW53ZWkgNTQ4MTA7RU5Hc2NvcGVJbnRyYW5ldA==";
 
-			System.out.println("Iterate all keys...");
-			// iterate all keys
-			KeyIterator it = memcachedClient.getKeyIterator(AddrUtil
-					.getOneAddress(args[0]));
-			while (it.hasNext()) {
-				System.out.println(it.next());
-			}
 
-		} catch (MemcachedException e) {
-			System.err.println("MemcachedClient operation fail");
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			System.err.println("MemcachedClient operation timeout");
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// ignore
-		}
-		try {
-			Thread.sleep(Integer.MAX_VALUE);
-			memcachedClient.shutdown();
-		} catch (Exception e) {
-			System.err.println("Shutdown MemcachedClient fail");
-			e.printStackTrace();
-		}
-	}
+    static String genKey(int len, Object v) {
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < len) {
+            sb.append(v);
+        }
+        return sb.toString();
+    }
 
-	public static MemcachedClient getMemcachedClient(String servers) {
-		try {
-			// use text protocol by default
-			MemcachedClientBuilder builder = new XMemcachedClientBuilder(
-					AddrUtil.getAddresses(servers));
-			return builder.build();
-		} catch (IOException e) {
-			System.err.println("Create MemcachedClient fail");
-			e.printStackTrace();
-		}
-		return null;
-	}
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Useage:java SimpleExample [servers]");
+            System.exit(1);
+        }
+        MemcachedClient memcachedClient = getMemcachedClient(args[0]);
+        if (memcachedClient == null) {
+            throw new NullPointerException("Null MemcachedClient,please check memcached has been started");
+        }
+        try {
+            // 设置一个长度超过250的key
+            System.out.println(memcachedClient.get(specilaKey));
+            memcachedClient.set(specilaKey, 0, 1);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            // 设置一个正常的key
+            memcachedClient.set(genKey(111, "b"), 0, 1);
+            // 查看取值是否正确
+            System.out.println("value:" + memcachedClient.get(genKey(111, "b")));
+            // Thread.sleep(Integer.MAX_VALUE);
+            memcachedClient.shutdown();
+        }
+        catch (Exception e) {
+            System.err.println("Shutdown MemcachedClient fail");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static MemcachedClient getMemcachedClient(String servers) {
+        try {
+            // use text protocol by default
+            MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(servers));
+            builder.setCommandFactory(new BinaryCommandFactory());
+            return builder.build();
+            // MemcachedClient client = new XMemcachedClient();
+            // client.addServer(servers);
+            // return client;
+        }
+        catch (IOException e) {
+            System.err.println("Create MemcachedClient fail");
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
